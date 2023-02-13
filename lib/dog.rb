7ce0b18036ec6827f58b114dad2d03432d9bab62
kennedy <kenneyrit@gmail.com>
2023-02-13 12:29:06 +0300

    done

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+#class dog
+class Dog
+  #initialize method
+  def initialize(name, breed = "Mutt")
+      @name=name
+      @breed=breed
+  end
+end 
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+#defining a person class
+class Person
+  #initialize method that accepts person's name as argument
+  def initialize(name)
+      @name=name
+  end
+end 
